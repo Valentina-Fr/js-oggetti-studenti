c44c848dbbb755e24b3894302d1805f9b96f91dc
Valentina-Fr <v_frasca@virgilio.it>
2021-07-19 15:20:13 +0200

    Add object student

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* 
+ES 1
+Creare un oggetto che descriva uno studente, 
+con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in 
+tutte le proprietà dell'oggetto.
+*/
+
+//Oggetto studente
+const student = {
+    name: 'John',
+    surname: 'Smith',
+    age: '40'
+}
+
+console.log(student);
